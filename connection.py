ceef5f56a26ee864154765dac4c603e13c57bcc2
Dark_ezt <54188061+Creativing-Byte@users.noreply.github.com>
2025-03-20 21:13:18 -0500

    conexion a bd

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://JuanGuzman:Hola123@sampleinformationservic.x31xs.mongodb.net/?retryWrites=true&w=majority&appName=SampleinformationService"
+    uri = "mongodb://JuanGuzman:Hola123@sampleinformationservic-shard-00-00.x31xs.mongodb.net:27017,sampleinformationservic-shard-00-01.x31xs.mongodb.net:27017,sampleinformationservic-shard-00-02.x31xs.mongodb.net:27017/?replicaSet=atlas-pdxqbo-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=SampleinformationService"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
